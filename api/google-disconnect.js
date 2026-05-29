af5f5b2014436a9eb9501274bdc0ec060932d429
chloesalkeld31 <chloesalkeld31@gmail.com>
2026-05-29 07:27:29 -0400

    Update google-disconnect.js

--- api/google-disconnect.js
+++ api/google-disconnect.js
@@ -1,5 +1,4 @@
-// Clear the Google Calendar cookie
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://family-dashboard-rho-nine.vercel.app')
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
